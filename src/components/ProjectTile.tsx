f20c378e25ea156606eb8bb07717dcc8abb185d8
felipecock <26395118+felipecock@users.noreply.github.com>
2022-03-18 01:40:59 -0500

    Create a new images set and optimize projects data

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,29 +1,31 @@
-import React, { lazy, MouseEventHandler, useState } from "react"
+import React from "react"
 import styles from "./ProjectTile.module.css"
 
 export interface ProjectTileProps {
   id?: string
   title: string
-  image: string | Array<[source: string, maxWidth: number]>
+  imageBaseName?: string
+  imageSource?: string | Array<[source: string, maxWidth: number]>
   description: string
   link: string
 }
 
 const ProjectTile: React.FunctionComponent<ProjectTileProps> = (props) => {
   const altSentence = "Vista previa de "
-  const imageSrcSet = Array.isArray(props.image) ? props.image.map((srcSet) => `${srcSet[0]} ${srcSet[1]}w`).join() : ""
+  const imageSrcSet = Array.isArray(props.imageSource) ? props.imageSource.map((srcSet) => `${srcSet[0]} ${srcSet[1]}w`).join() : ""
+  const defaultImage = '%PUBLIC_URL%/favicon.svg'
 
   return (
     <div className={styles.projectTile}>
       <h2 className={styles.projectTitle}>{props.title}</h2>
       <div className={styles.gridPreviewImage}>
-        {Array.isArray(props.image) ? (
-          <>
+        {Array.isArray(props?.imageSource) ? (
+          <object data={defaultImage} type={"image/svg+xml"}>
             <img
               id={props.id}
               className={styles.gridPreviewImage}
               alt={altSentence + props.title}
-              src={props.image[0][0]}
+              src={props.imageSource[0][0]}
               srcSet={imageSrcSet}
               sizes={"(max-width: 481px) 40vw, (min-width: 481px) 20vw, (min-width: 769px) 14vw"}
               width={213}
@@ -34,14 +36,14 @@ const ProjectTile: React.FunctionComponent<ProjectTileProps> = (props) => {
               id={props.id + "-large"}
               className={styles.gridPreviewImageLarge}
               alt={altSentence + props.title}
-              src={props.image[0][0]}
+              src={props.imageSource[0][0]}
               srcSet={imageSrcSet}
               sizes={"(max-width: 481px) 90vw, (min-width: 481px) 40vw, (min-width: 769px) 30vw"}
               loading={"lazy"}
             />
-          </>
+          </object>
         ) : (
-          <img className={styles.gridPreviewImage} alt={altSentence + props.title} src={props.image} />
+          <img className={styles.gridPreviewImage} alt={altSentence + props.title} src={props.imageSource} />
         )}
       </div>
       <p className={styles.project}>{props.description}</p>
@@ -50,13 +52,6 @@ const ProjectTile: React.FunctionComponent<ProjectTileProps> = (props) => {
       </a>
     </div>
   )
-}
-
-ProjectTile.defaultProps = {
-  title: "Project",
-  image: "",
-  description: "It is a new project",
-  link: "",
 }
 
 export default ProjectTile
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,78 +1,22 @@
 import React from "react"
 import styles from "./Projects.module.css"
 import ProjectTile, { ProjectTileProps } from "./ProjectTile"
+import ProjectList from "../json/projectList.json"
 
-const projectList: Array<ProjectTileProps> = [
-  {
-    title: "Proyecto ASILOS",
-    image: [
-      ["./images/screenshots/optimized/asilos-project-120.png", 213],
-      ["./images/screenshots/optimized/asilos-project-240.png", 426],
-      ["./images/screenshots/optimized/asilos-project-360.png", 640],
-      ["./images/screenshots/optimized/asilos-project-480.png", 854],
-    ],
-    description: "Proyecto ASILOS: Archivo de Soluciones a Inconvenientes en Linux u Otro Software",
-    link: "https://felipecock.github.io/ASILOS/main.html",
-  },
-  {
-    title: "Sitio Web de GENCORE S.A.S.",
-    image: [
-      ["./images/screenshots/optimized/gencore-page-120.png", 213],
-      ["./images/screenshots/optimized/gencore-page-240.png", 426],
-      ["./images/screenshots/optimized/gencore-page-360.png", 640],
-      ["./images/screenshots/optimized/gencore-page-480.png", 854],
-    ],
-    description:
-      "Sitio web en HTML5 y CSS puro, con diseño web adaptable e interfaz de usuario limpia, intuitiva y amigable",
-    link: "https://codepen.io/felipecock/full/QWNEPOE",
-  },
-  {
-    title: "Formulario Web Adaptativo",
-    image: [
-      ["./images/screenshots/optimized/adaptive-web-form-120.png", 213],
-      ["./images/screenshots/optimized/adaptive-web-form-240.png", 426],
-      ["./images/screenshots/optimized/adaptive-web-form-360.png", 640],
-      ["./images/screenshots/optimized/adaptive-web-form-480.png", 854],
-    ],
-    description:
-      "Formulario en HTML5 y CSS con diseño web adaptativo, con opciones de accesibilidad, interfaz limpia, agradable e intuitiva",
-    link: "https://codepen.io/felipecock/full/zYqqqMr",
-  },
-  {
-    title: "Python Arduino Control GUI",
-    image: [
-      ["./images/screenshots/optimized/gui-monitor-python-120.png", 213],
-      ["./images/screenshots/optimized/gui-monitor-python-240.png", 426],
-      ["./images/screenshots/optimized/gui-monitor-python-360.png", 640],
-      ["./images/screenshots/optimized/gui-monitor-python-480.png", 854],
-    ],
-    description:
-      "Software en Python2.7 y Arduino con interfaz de usuario para monitorear y controlar en tiempo real un Arduino UNO",
-    link: "https://github.com/felipecock/Python-Arduino-Control-GUI",
-  },
-  {
-    title: "Página Biográfica en HTML5 y CSS",
-    image: [
-      ["./images/screenshots/optimized/memorial-120.png", 213],
-      ["./images/screenshots/optimized/memorial-240.png", 426],
-      ["./images/screenshots/optimized/memorial-360.png", 640],
-      ["./images/screenshots/optimized/memorial-480.png", 854],
-    ],
-    description: "Página Tributo a San Ignacio de Loyola destacando los principales aspectos de su vida",
-    link: "https://codepen.io/felipecock/full/zYqrrde",
-  },
-  {
-    title: "Sitio Web de Writeduca",
-    image: [
-      ["./images/screenshots/optimized/writeduca-120.png", 213],
-      ["./images/screenshots/optimized/writeduca-240.png", 426],
-      ["./images/screenshots/optimized/writeduca-360.png", 640],
-      ["./images/screenshots/optimized/writeduca-480.png", 854],
-    ],
-    description: "Sitio web responsive desarrollado en Google Sites con componentes personalizados.",
-    link: "https://www.writeduca.com/",
-  },
-]
+const projectList: Array<ProjectTileProps> = ProjectList
+
+const imagesPath = "./images/screenshots/optimized/"
+const resolutions = [120, 240, 360, 480, 600, 720, 800]
+const imageFormat = ".png"
+
+projectList.forEach((project) => {
+  if (!project?.imageSource && !!project.imageBaseName) {
+    project.imageSource = resolutions.map((resolution) => [
+      imagesPath + project.imageBaseName + "-" + resolution.toString() + imageFormat,
+      resolution,
+    ])
+  }
+})
 
 export const Projects = () => {
   const generateTiles = () =>
@@ -83,7 +27,7 @@ export const Projects = () => {
           key={tile_id}
           id={tile_id}
           title={project.title}
-          image={project.image}
+          imageSource={project.imageSource}
           description={project.description}
           link={project.link}></ProjectTile>
       )
