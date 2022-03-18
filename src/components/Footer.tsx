09274116874484b31b4d59747209d40907e6faaf
felipecock <26395118+felipecock@users.noreply.github.com>
2022-03-17 21:18:25 -0500

    Update footer stylesheet

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,26 @@
 import React from "react"
 import { ReactComponent as GithubIcon } from "../images/icons/github-icon.svg"
+import styles from "./Footer.module.css"
 
 export const Footer = () => {
   return (
     <footer>
-      <div className="contact-info">
-        <p className="contact-info">
+      <div className={styles.contactInfo}>
+        <p className={styles.contactInfo}>
           <strong>Felipe Cock</strong>
         </p>
-        <p className="contact-info">Desarrollador Front-End</p>
-        <p className="contact-info">
+        <p className={styles.contactInfo}>Desarrollador Front-End</p>
+        <p className={styles.contactInfo}>
           <a
-            className="contact-info"
+            className={styles.contactInfo}
             href="mailto:felipecock@gmail.com?subject=Nuevo Proyecto&body=Buenos días Felipe.%0D%0AEstamos trabajando en un nuevo proyecto y queremos que seas parte de nuestro equipo de desarrollo.">
             felipecock@gmail.com
           </a>
         </p>
-        <p className="contact-info">Medellín, Colombia</p>
-        <p className="contact-info">
-          <a className="contact-info" target="_blank" rel="noreferrer" href="https://github.com/felipecock">
-            <GithubIcon />
-            <span className="content-info">GitHub @felipecock</span>
+        <p className={styles.contactInfo}>Medellín, Colombia</p>
+        <p className={styles.contactInfo}>
+          <a className={styles.contactInfo} target="_blank" rel="noreferrer" href="https://github.com/felipecock">
+            <GithubIcon /> GitHub @felipecock
           </a>
         </p>
         <p className="copyright">Copyright© 2022 Felipe Cock</p>
